528495ae4cdd753eac93c8cc9935ffdc20c949da
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2026-04-25 04:12:20 +0500

    Remove skypack pkgs

--- client/src/mermaid.ts
+++ client/src/mermaid.ts
@@ -1,8 +1,7 @@
-import Mermaid from 'mermaid';
+import type { Mermaid } from 'mermaid';
 import { getInjectConfig } from './util.ts';
 
-// TODO: replace skypack cdn mermaid with type from original source
-declare const mermaid: typeof Mermaid;
+declare const mermaid: Mermaid;
 
 function init() {
   const peek = getInjectConfig();
